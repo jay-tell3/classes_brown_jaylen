18119c89bbade009ad73ea9bae84dc4f28623fbf
jay-tell3 <jaybroww@gmail.com>
2025-04-03 13:18:44 -0500

    done

--- Program.cs
+++ Program.cs
@@ -3,8 +3,16 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   //creates an object frome the hi class
+            hi hi = new hi();
+            //calls the welcome methed from the hi class 
+            hi.welcome();
+            //asks for a name
+            Console.WriteLine("what u name is?");
+            // makes a string called username and it is set to what the user inputs
+            string username = Console.ReadLine();
+            //calls the hello methed from the hi class with the username varable used in place of name
+            hi.Hello(username);
         }
     }
 }
---
+++ hi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_brown_jaylen
+{//makes the class
+    internal class hi
+    {
+        //makes a method that can be called useing the class and method name
+        public void welcome()
+        {//says hi
+            Console.WriteLine("hi person");
+        }
+        //makes a method that can be called useing the class and method name
+        public static void Hello(string name)
+        {//says hello and the users username varable input
+         Console.WriteLine($"Hello {name}");
+        }
+    }
+}
